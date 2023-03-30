e0f46dd2bfa0b6b84128fd3daecf4e0321da3eb0
MrsUser <MrsUser@NOOKIE>
2023-03-29 20:06:25 -0500

    Add project files.

---
+++ ClassLibrary1/RickAndBob.cs
@@ -0,0 +1,57 @@
+﻿namespace RickAndBobLib
+{
+
+    public interface IRickAndBob
+    {
+        List<string> GetRickAndBobifiedList(int listLength);
+
+    }
+    public class RickAndBob : IRickAndBob
+    {
+
+        public List<string>  GetRickAndBobifiedList(int listLength)
+        {
+            List<string> l = new List<string>();
+
+            const int rickyNumber = 3;
+            const int bobbyNumber = 5;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i < rickyNumber)
+                {
+                    l.Add(i.ToString());
+                }
+                else
+                {
+
+                    var ricky = i % rickyNumber;
+                    var bobby = i % bobbyNumber;
+
+
+                    string currentLine = string.Empty; 
+                    if (ricky == 0 || bobby == 0)
+                    {
+                        
+                        if (ricky == 0)
+                        {
+                            currentLine += "Ricky ";
+                        }
+                        if (bobby == 0)
+                        {
+                            currentLine += "Bobby ";
+                        }
+                        l.Add(currentLine);
+                    }
+                    else
+                    {
+                        l.Add(i.ToString());
+                    }
+                }
+
+            }
+            return l;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using RickAndBobLib;
+
+
+
+IRickAndBob sharedLib = new RickAndBob();
+var result = sharedLib.GetRickAndBobifiedList(100);
+
+//  The Console.WriteLine() was not included in the class library to eliminate dependencies allowing each project to deal with the output in it's own way.
+//  We could also modify this method to take additional parameters such as counts other than 100 or new inputs(Name, Number) instead of hardcoding Ricky-3 and Bobby-5 but this was not specified
+//  Publishing to private Nuget server is a great way to share class libraries across projects
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+
+Console.ReadKey();
+
+
+
+
